662e6b9db8d1a9d4178e8868bf16e12cf220ff02
scavengerhunta1 <83606245+scavengerhunta1@users.noreply.github.com>
2021-05-04 00:25:27 +0500

    Update answers.js

--- answers.js
+++ answers.js
@@ -18,7 +18,7 @@ function check_ans2(){
   var answer = document.querySelector("#answer");
   var wrong = document.querySelector("#wrong");
   var next = document.querySelector(".next");
-  if (answer.value == "Wonder Woman" || answer.value == "wonder woman" || answer.value == "aquaman" || answer.value == "Aquaman" || answer.value == "Wonder woman" && answer2.value=="Aquaman" || answer2.value == "Wonder Woman" || answer2.value == "wonder woman" || answer2.value == "aquaman" || answer2.value == "Wonder woman"){
+  if (answer.value == "Edgar Allan" || answer.value == "Edgar allan" || answer.value == "edgar allan"){
     right.style.display = "block";
     next.style.display = "inline";
     wrong.style.display = "none";
@@ -29,21 +29,7 @@ function check_ans2(){
     next.style.display = "none";
   }
 }
-function check_ans3(){
-  var right = document.querySelector("#right");
-  var answer = document.querySelector("#answer");
-  var wrong = document.querySelector("#wrong");
-  var next = document.querySelector(".next");
-  if (answer.value == "Grievous" || answer.value == "grievous"){
-    next.style.display = "inline";
-    wrong.style.display = "none"
-  }
-  else{
-    wrong.style.display = "block"
-    right.style.display = "none";
-    next.style.display = "none";
-  }
-}
+
 
 
 function check_ans3(){
