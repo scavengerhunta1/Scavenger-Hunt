f11da47622dfcb875f8af6bf8ea8df6a65f05bcf
scavengerhunta1 <83606245+scavengerhunta1@users.noreply.github.com>
2021-05-03 23:15:35 +0500

    Update answers.js

--- answers.js
+++ answers.js
@@ -3,7 +3,7 @@ function check_ans1(){
   var answer = document.querySelector("#answer");
   var wrong = document.querySelector("#wrong");
   var next = document.querySelector(".next");
-  if (answer.value == "Thor Ragnarok" || answer.value == "thor ragnarok" || answer.value == "Thor ragnarok" || answer.value == "Thor: ragnarok" || answer.value == "Thor: Ragnarok" || answer.value == "Thor:Ragnarok" || answer.value == "Thor:ragnarok"){
+  if (answer.value == "Famous Five" || answer.value == "famous five" || answer.value == "Famous five" || answer.value == "The famous five" || answer.value == "The Famous Five"){
     next.style.display = "inline";
     wrong.style.display = "none"
   }
