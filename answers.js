c9b5f9501513c986c03c77ef09d3b23f0a6bb854
scavengerhunta1 <83606245+scavengerhunta1@users.noreply.github.com>
2021-05-03 23:14:18 +0500

    Update answers.js

--- answers.js
+++ answers.js
@@ -1,1 +1,62 @@
+function check_ans1(){
+  var right = document.querySelector("#right");
+  var answer = document.querySelector("#answer");
+  var wrong = document.querySelector("#wrong");
+  var next = document.querySelector(".next");
+  if (answer.value == "Thor Ragnarok" || answer.value == "thor ragnarok" || answer.value == "Thor ragnarok" || answer.value == "Thor: ragnarok" || answer.value == "Thor: Ragnarok" || answer.value == "Thor:Ragnarok" || answer.value == "Thor:ragnarok"){
+    next.style.display = "inline";
+    wrong.style.display = "none"
+  }
+  else{
+    wrong.style.display = "block"
+    right.style.display = "none";
+    next.style.display = "none";
+  }
+}
+function check_ans2(){
+  var right = document.querySelector("#right");
+  var answer = document.querySelector("#answer");
+  var wrong = document.querySelector("#wrong");
+  var next = document.querySelector(".next");
+  if (answer.value == "Wonder Woman" || answer.value == "wonder woman" || answer.value == "aquaman" || answer.value == "Aquaman" || answer.value == "Wonder woman" && answer2.value=="Aquaman" || answer2.value == "Wonder Woman" || answer2.value == "wonder woman" || answer2.value == "aquaman" || answer2.value == "Wonder woman"){
+    right.style.display = "block";
+    next.style.display = "inline";
+    wrong.style.display = "none";
+  }
+  else{
+    wrong.style.display = "block";
+    right.style.display = "none";
+    next.style.display = "none";
+  }
+}
+function check_ans3(){
+  var right = document.querySelector("#right");
+  var answer = document.querySelector("#answer");
+  var wrong = document.querySelector("#wrong");
+  var next = document.querySelector(".next");
+  if (answer.value == "Grievous" || answer.value == "grievous"){
+    next.style.display = "inline";
+    wrong.style.display = "none"
+  }
+  else{
+    wrong.style.display = "block"
+    right.style.display = "none";
+    next.style.display = "none";
+  }
+}
 
+
+function check_ans3(){
+  var right = document.querySelector("#right");
+  var answer = document.querySelector("#answer");
+  var wrong = document.querySelector("#wrong");
+  var next = document.querySelector(".next");
+  if (answer.value == "Redbeard" || answer.value == "redbeard" || answer.value == "Red beard" || answer.value == "red beard"){
+    next.style.display = "inline";
+    wrong.style.display = "none"
+  }
+  else{
+    wrong.style.display = "block"
+    next.style.display = "none";
+  }
+}
